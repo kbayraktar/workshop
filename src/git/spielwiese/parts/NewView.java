7a06983b7770b3c3e804d9229f2d69132a60d847
holl3 <matthias.holsten@gmail.com>
2014-07-01 11:45:28 +0200

    Create an the new View "newView"
    * Add javax.annotations to MANIFEST
    * New Class NewView
    * add NewView to Apllication.xmi

---
+++ src/git/spielwiese/parts/NewView.java
@@ -0,0 +1,22 @@
+ 
+package git.spielwiese.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class NewView {
+	@Inject
+	public NewView() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
